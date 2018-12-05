1b7eb9a22c1822b8e94d900b0d3478253697595d
Joseph <joembaker@berkeley.edu>
2018-12-05 14:56:53 -0800

    Test For Pub_Sub

--- ar_track_pub.py
+++ ar_track_pub.py
@@ -7,6 +7,9 @@ from geometry_msgs.msg import PoseStamped
 from moveit_commander import MoveGroupCommander
 from intera_interface import gripper as robot_gripper
 import tf2_ros
+from std_msgs.msg import String
+from std_msgs.msg import Float64
+from std_msgs.msg import Float64MultiArray
 
 def ar_track(last_frame):
 
@@ -16,11 +19,21 @@ def ar_track(last_frame):
 	ar_to_cb = np.array([.1, .1, 0])
 	current_frame = cam_to_ar - ar_to_cb
 	if 	sum(abs(current_frame - last_frame)) >= .02:
-		origin_devation = current_frame - last_frame
-		return(current_frame,origin_devation)
+		origin_deviation = current_frame - last_frame
+		return(current_frame,origin_deviation)
 
 	else:
-		return(current_frame,[0, 0, 0])
+		origin_deviation = [0,0,0]
+		return(current_frame,origin_deviation)
+
+	#  to sprintf in C or MATLAB)
+	pub_origin_deviation = origin_deviation
+
+	# Publish our string to the 'origin_deviation' topic
+	origin_deviation.publish(pub_origin_deviation)
+
+	# Use our rate object to sleep until it is time to publish again
+	r.sleep()
 
 #Python's syntax for a main() method
 def main():
@@ -33,8 +46,13 @@ if __name__ == '__main__':
 	rospy.sleep(1)
 	tfListener = tf2_ros.TransformListener(tfBuffer) 
 	rospy.sleep(1)
+	
+	origin_deviation = rospy.Publisher('origin_deviation', Float64MultiArray, queue_size=10)
+	r = rospy.Rate(10)
+
+
 	test = ar_track([0,0,0])
 	current_frame = test[0]
-	origin_devation = test[1]
+	origin_deviation = test[1]
 	print(current_frame)
-	print(origin_devation)
+	print(origin_deviation)
